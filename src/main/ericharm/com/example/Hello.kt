86a2af85260eeb5e0861f5fa16183e19264206d5
Eric Harm <ericharm@gmail.com>
2020-11-02 00:17:06 -0500

    curses running on gradle

--- src/main/ericharm/com/example/Hello.kt
+++ src/main/ericharm/com/example/Hello.kt
@@ -1,14 +1,35 @@
 package com.example
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.TextColor
+import com.googlecode.lanterna.gui2.*
+import com.googlecode.lanterna.screen.Screen
+import com.googlecode.lanterna.screen.TerminalScreen
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import com.googlecode.lanterna.terminal.Terminal
 
 fun main(args: Array<String>) {
-  println("Hello Gradle!")
-  println("Build          : gradle build")
-  println("Clean          : gradle clean")
-  println("List tasks     : gradle tasks --all")
-  println("Run main       : gradle run")
-  println("Run test       : gradle test")
-  println("Run getDouble  : gradle getDouble")
-  println("Run getSquare  : gradle getSquare")
-  println("Run getRandom  : gradle getRandom")
-  println("Run mulitasks  : gradle run getDouble getSquare getRandom")
+    val terminal = DefaultTerminalFactory().createTerminal()
+    val screen = TerminalScreen(terminal)
+    screen.startScreen()
+
+    // Create panel to hold components
+    val panel = Panel()
+    panel.setLayoutManager(GridLayout(2))
+
+    panel.addComponent(Label("Forename"))
+    panel.addComponent(TextBox())
+
+    panel.addComponent(Label("Surname"))
+    panel.addComponent(TextBox())
+
+    panel.addComponent(EmptySpace(TerminalSize(0,0)))
+    panel.addComponent(Button("Submit"));
+
+    // Create window to hold the panel
+    val window = BasicWindow()
+    window.setComponent(panel)
+
+    // Create gui and start gui
+    val gui = MultiWindowTextGUI(screen, DefaultWindowManager(), EmptySpace(TextColor.ANSI.BLUE))
+    gui.addWindowAndWait(window)
 }
