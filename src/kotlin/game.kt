d129554f73fb39f56836a12b33fb9e9a43e148d6
Eric Harm <ericharm@gmail.com>
2020-11-02 22:10:45 -0500

    generalize hero into entity, create hero with object declaration

--- src/kotlin/game.kt
+++ src/kotlin/game.kt
@@ -6,19 +6,21 @@ import com.googlecode.lanterna.TerminalSize
 import com.googlecode.lanterna.terminal.Terminal
 import com.googlecode.lanterna.input.KeyType
 
-fun TextGraphics.drawRectangle(position: TerminalPosition, size: TerminalSize, horizontalChar: Char,verticalChar: Char) {
-    val width = size.columns
-    val height = size.rows
-    val x = position.column
-    val y = position.row
-    val bottom = TerminalPosition(x, position.row + size.rows - 1)
-    drawRectangle(position, size, verticalChar)
-    putString(position, horizontalChar.toString().repeat(width))
-    putString(bottom, horizontalChar.toString().repeat(width))
-}
-
 class Game() {
-    val hero = Hero(Pair(10,10))
+    object hero : Entity(Pair(10, 10)) {
+        override val character = "@"
+    }
+
+    fun TextGraphics.drawRectangle(position: TerminalPosition, size: TerminalSize, horizontalChar: Char,verticalChar: Char) {
+        val width = size.columns
+        val height = size.rows
+        val x = position.column
+        val y = position.row
+        val bottom = TerminalPosition(x, position.row + size.rows - 1)
+        drawRectangle(position, size, verticalChar)
+        putString(position, horizontalChar.toString().repeat(width))
+        putString(bottom, horizontalChar.toString().repeat(width))
+    }
 
     fun render(screen: TerminalScreen) {
         val tg = screen.newTextGraphics()
